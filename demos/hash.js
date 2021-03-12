83a4437fb7d3f6117ffca2e34628c9d94b7520f0
Shnurok1342 <a13bepb@yandex.ru>
2021-03-12 18:54:37 +0300

    feat: Learn Offline Dictionary Attacks - Why Use Cryptographic Salting?

--- demos/hash.js
+++ demos/hash.js
@@ -2,9 +2,12 @@ var crypto = require('crypto');
 
 var password = 'monkey1';
 
+var aliceSalt = 1;
+var bobSalt = 2;
+
 var hash = crypto
   .createHash('sha256')
-  .update(password)
+  .update(password + aliceSalt)
   .digest('hex');
 
 console.log(`The result of hashing ${password} is:`);
