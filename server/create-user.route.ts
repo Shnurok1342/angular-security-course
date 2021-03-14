e2c587c854364bc236ed5809978696fce40047c2
Shnurok1342 <a13bepb@yandex.ru>
2021-03-14 22:35:05 +0300

    (JWT) feat: User Sign Up With JWT - Learn How To Combine Cookies and JWTs

--- server/create-user.route.ts
+++ server/create-user.route.ts
@@ -2,8 +2,7 @@ import {Request, Response} from 'express';
 import {db} from './database';
 import * as argon2 from 'argon2';
 import {validatePassword} from './password-validation';
-import {randomBytes} from './security.utils';
-import {sessionStore} from './session-store';
+import {createSessionToken, randomBytes} from './security.utils';
 
 export function createUser(req: Request, res: Response) {
   const credentials = req.body;
@@ -21,8 +20,8 @@ async function createUserAndSession(res: Response, credentials) {
     const user = db.createUser(credentials.email, passwordDigest);
     const sessionId = await randomBytes(32).then(bytes => bytes.toString('hex'));
     console.log('sessionId:', sessionId);
-    sessionStore.createSession(sessionId, user);
-    res.cookie('SESSION_ID', sessionId, { httpOnly: true, secure: true });
+    const sessionToken = await createSessionToken(user.id.toString());
+    res.cookie('SESSION_ID', sessionToken, { httpOnly: true, secure: true });
     res.status(200).json({id: user.id, email: user.email});
   } catch (e) {
     console.log(e.message);
--- server/security.utils.ts
+++ server/security.utils.ts
@@ -1,4 +1,21 @@
 const util = require('util');
-const cryptoP = require('crypto');
+const crypto = require('crypto');
+import * as jwt from 'jsonwebtoken';
+import * as fs from 'fs';
 
-export const randomBytes = util.promisify(cryptoP.randomBytes);
+const moment = require('moment');
+
+export const randomBytes = util.promisify(crypto.randomBytes);
+export const signJwn = util.promisify(jwt.sign);
+
+const RSA_PRIVATE_KEY = fs.readFileSync('./demos/JWT/private.key');
+const RSA_PUBLIC_KEY = fs.readFileSync('./demos/JWT/public.key');
+const SESSION_DURATION = 240;
+
+export async function createSessionToken(userId: string) {
+  return signJwn({}, RSA_PRIVATE_KEY, {
+    algorithm: 'RS256',
+    expiresIn: SESSION_DURATION,
+    subject: userId
+  });
+}
